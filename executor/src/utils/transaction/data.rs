cccfb16763b4c8089f355d528f042fe48a8b2d85
Fusee <quentus123@orange.fr>
2023-10-02 17:52:22 +0200

    fixed publish issues for executor crate

--- executor/src/utils/transaction/data.rs
+++ executor/src/utils/transaction/data.rs
@@ -1,9 +1,10 @@
+use std::str::FromStr;
 use multiversx_sc::api::CallTypeApi;
 use multiversx_sc::types::ContractCallWithEgld;
 use multiversx_sc_scenario::scenario_model::{ScCallStep, ScDeployStep, TxCall, TxDeploy, TypedScCall, TypedScDeploy};
 use multiversx_sdk::data::address::Address;
 use num_bigint::BigUint;
-use serde::{Deserialize, Serialize};
+use serde::{de, Deserialize, Serialize, Serializer};
 
 /// Represents a transaction that is ready to be sent to the blockchain.
 ///
@@ -14,6 +15,8 @@ pub struct SendableTransaction {
     /// The receiver's address as a string.
     pub receiver: String,
     /// The amount of EGLD to be sent along with the transaction.
+    #[serde(serialize_with = "biguint_serialize")]
+    #[serde(deserialize_with = "biguint_deserialize")]
     pub egld_value: BigUint,
     /// The gas limit for the transaction.
     pub gas_limit: u64,
@@ -120,4 +123,56 @@ fn contract_call_to_tx_data<SA, T>(contract_call: &ContractCallWithEgld<SA, T>) 
         result.push_str(hex::encode(argument.to_boxed_bytes().as_slice()).as_str());
     }
     result
+}
+
+fn biguint_serialize<S>(value: &BigUint, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer
+{
+    serializer.serialize_str(&value.to_string())
+}
+
+fn biguint_deserialize<'de, D>(deserializer: D) -> Result<BigUint, D::Error>
+where
+    D: de::Deserializer<'de>
+{
+    let string = String::deserialize(deserializer)?;
+
+    Ok(BigUint::from_str(&string).unwrap())
+}
+
+#[cfg(test)]
+mod tests {
+    use num_bigint::BigUint;
+    use crate::SendableTransaction;
+
+    #[test]
+    fn test_serialize_sendable_transaction() {
+        let tx = SendableTransaction {
+            receiver: "erd1qqqqqqqqqqqqqpgq74myhunu4sfdpmskm6s6ul8k4cetjvhhlfpsaa20la".to_string(),
+            egld_value: BigUint::from(10u8).pow(18),
+            gas_limit: 600000000,
+            data: "@6f6b@000000080de0b6b3a7640000000000081bc16d674ec80000".to_string(),
+        };
+
+        let result = serde_json::to_string(&tx).unwrap();
+        let expected = r#"{"receiver":"erd1qqqqqqqqqqqqqpgq74myhunu4sfdpmskm6s6ul8k4cetjvhhlfpsaa20la","egld_value":"1000000000000000000","gas_limit":600000000,"data":"@6f6b@000000080de0b6b3a7640000000000081bc16d674ec80000"}"#;
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_deserialize_sendable_transaction() {
+        let json = r#"{"receiver":"erd1qqqqqqqqqqqqqpgq74myhunu4sfdpmskm6s6ul8k4cetjvhhlfpsaa20la","egld_value":"1000000000000000000","gas_limit":600000000,"data":"@6f6b@000000080de0b6b3a7640000000000081bc16d674ec80000"}"#;
+
+        let result: SendableTransaction = serde_json::from_str(json).unwrap();
+        let expected = SendableTransaction {
+            receiver: "erd1qqqqqqqqqqqqqpgq74myhunu4sfdpmskm6s6ul8k4cetjvhhlfpsaa20la".to_string(),
+            egld_value: BigUint::from(10u8).pow(18),
+            gas_limit: 600000000,
+            data: "@6f6b@000000080de0b6b3a7640000000000081bc16d674ec80000".to_string(),
+        };
+
+        assert_eq!(result, expected);
+    }
 }
