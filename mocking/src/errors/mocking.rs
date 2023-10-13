0fdc521d24a353b8d7b1355e45b287d70cb9b01a
Fusee <quentus123@orange.fr>
2023-10-13 16:09:37 +0200

    improved error handling when cloning the blockchain

--- mocking/src/errors/mocking.rs
+++ mocking/src/errors/mocking.rs
@@ -1,16 +1,24 @@
 use serde::{Deserialize, Serialize};
 use novax::errors::NovaXError;
+use novax_token::error::token::TokenError;
 
 #[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
 pub enum NovaXMockingError {
     UnableToFetchAddressKeys,
     UnableToParseAddressKeys,
     UnableToReadInfosFromFile,
-    NovaXError(NovaXError)
+    NovaXError(NovaXError),
+    NovaXTokenError(TokenError)
 }
 
 impl From<NovaXError> for NovaXMockingError {
     fn from(value: NovaXError) -> Self {
         NovaXMockingError::NovaXError(value)
+    }
+}
+
+impl From<TokenError> for NovaXMockingError {
+    fn from(value: TokenError) -> Self {
+        NovaXMockingError::NovaXTokenError(value)
     }
 }
