7d55d12dbecef8cfe04335c146f97840098e861f
Etienne Wallet <116115319+EtienneWallet@users.noreply.github.com>
2024-08-18 16:55:52 +0200

    fix: typo

--- account/src/account/info.rs
+++ account/src/account/info.rs
@@ -132,7 +132,7 @@ async fn fetch_account_info_for_address<Client, Caching>(gateway_client: &Client
             };
 
             let code_metadata = if let Some(raw_code_metadata) = raw_info.code_metadata {
-                Some(decode_code_meta_data(raw_code_metadata)?)
+                Some(decode_code_metadata(raw_code_metadata)?)
             } else {
                 None
             };
@@ -152,7 +152,7 @@ async fn fetch_account_info_for_address<Client, Caching>(gateway_client: &Client
     ).await
 }
 
-pub fn decode_code_meta_data(encoded: String) -> Result<CodeMetadata, AccountError> {
+pub fn decode_code_metadata(encoded: String) -> Result<CodeMetadata, AccountError> {
     let decoded_bytes = base64::engine::general_purpose::STANDARD.decode(encoded.clone()).or(Err(AccountError::CannotDecodeCodeMetadata { metadata: encoded.clone() }))?;
     if decoded_bytes.len() != 2 {
         return Err(AccountError::CannotDecodeCodeMetadata { metadata: encoded });
