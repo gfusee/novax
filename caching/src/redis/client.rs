9f78ac69e5b7d1a0d7a8aef415c9336dc26c648c
quentin <quentus123@gmail.com>
2025-04-26 01:06:14 +0200

    Forwarded description of redis caching errors

--- caching/src/redis/client.rs
+++ caching/src/redis/client.rs
@@ -1,8 +1,8 @@
 use std::fmt::Debug;
 
 use async_trait::async_trait;
-use redis::{AsyncConnectionConfig, FromRedisValue, IntoConnectionInfo, SetExpiry, ToRedisArgs};
 use redis::AsyncCommands;
+use redis::{AsyncConnectionConfig, FromRedisValue, IntoConnectionInfo, SetExpiry, ToRedisArgs};
 
 use crate::redis::error::CachingRedisError;
 
@@ -19,46 +19,46 @@ impl RedisClient for redis::Client {
     fn open<Info: IntoConnectionInfo>(info: Info) -> Result<Self, CachingRedisError> {
         match Self::open(info) {
             Ok(client) => Ok(client),
-            Err(_) => Err(CachingRedisError::CannotOpenConnection)
+            Err(error) => Err(CachingRedisError::CannotOpenConnection { description: format!("{}", error) })
         }
     }
 
     async fn set<K: ToRedisArgs + Send + Sync, V: ToRedisArgs + Send + Sync>(&self, key: K, value: V, duration: u64) -> Result<(), CachingRedisError> {
-        let Ok(mut connection) = self.get_multiplexed_async_connection_with_config(&AsyncConnectionConfig::new()).await else {
-            return Err(CachingRedisError::CannotGetConnection)
+        let mut connection = match self.get_multiplexed_async_connection_with_config(&AsyncConnectionConfig::new()).await {
+            Ok(connection) => connection,
+            Err(error) => return Err(CachingRedisError::CannotGetConnection { description: format!("{}", error) })
         };
 
         let options = redis::SetOptions::default()
             .with_expiration(SetExpiry::EX(duration));
 
-        if connection.set_options::<_, _, ()>(key, value, options).await.is_err() {
-            return Err(CachingRedisError::CannotSetValue)
+        match connection.set_options::<_, _, ()>(key, value, options).await {
+            Ok(_) => Ok(()),
+            Err(error) => Err(CachingRedisError::CannotSetValue { description: format!("{}", error) })
         }
-
-        return Ok(())
     }
 
     async fn get<K: ToRedisArgs + Send + Sync, RV: FromRedisValue + Send + Sync>(&self, key: K) -> Result<Option<RV>, CachingRedisError> {
-        let Ok(mut connection) = self.get_multiplexed_async_connection_with_config(&AsyncConnectionConfig::new()).await else {
-            return Err(CachingRedisError::CannotGetConnection)
+        let mut connection = match self.get_multiplexed_async_connection_with_config(&AsyncConnectionConfig::new()).await {
+            Ok(connection) => connection,
+            Err(error) => return Err(CachingRedisError::CannotGetConnection { description: format!("{}", error) })
         };
 
-        let Ok(value) = connection.get(key).await else {
-            return Err(CachingRedisError::CannotGetValue)
-        };
-
-        Ok(value)
+        match connection.get(key).await {
+            Ok(value) => Ok(value),
+            Err(error) => Err(CachingRedisError::CannotGetValue { description: format!("{}", error) })
+        }
     }
 
     async fn clear(&self) -> Result<(), CachingRedisError> {
-        let Ok(mut connection) = self.get_multiplexed_async_connection_with_config(&AsyncConnectionConfig::new()).await else {
-            return Err(CachingRedisError::CannotGetConnection)
+        let mut connection = match self.get_multiplexed_async_connection_with_config(&AsyncConnectionConfig::new()).await {
+            Ok(connection) => connection,
+            Err(error) => return Err(CachingRedisError::CannotGetConnection { description: format!("{}", error) })
         };
 
-        if redis::cmd("FLUSHALL").exec_async(&mut connection).await.is_err() {
-            return Err(CachingRedisError::CannotSetValue)
-        };
-
-        return Ok(());
+        match redis::cmd("FLUSHALL").exec_async(&mut connection).await {
+            Ok(_) => Ok(()),
+            Err(error) => Err(CachingRedisError::CannotClearAllValues { description: format!("{}", error) })
+        }
     }
 }
