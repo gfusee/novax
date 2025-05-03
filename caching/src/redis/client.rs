5d29fe9eed9e19eba850b20766dde151fc97f821
quentin <quentus123@gmail.com>
2025-05-03 19:58:32 +0200

    Use a single connection for redis caching

--- caching/src/redis/client.rs
+++ caching/src/redis/client.rs
@@ -1,26 +1,41 @@
-use std::fmt::Debug;
-
+use crate::redis::error::CachingRedisError;
 use async_trait::async_trait;
+use redis::aio::MultiplexedConnection;
 use redis::AsyncCommands;
 use redis::{AsyncConnectionConfig, FromRedisValue, IntoConnectionInfo, SetExpiry, ToRedisArgs};
-
-use crate::redis::error::CachingRedisError;
+use std::fmt::Debug;
 
 #[async_trait]
 pub trait RedisClient: Clone + Debug + Send + Sync {
-    fn open<Info: IntoConnectionInfo>(info: Info) -> Result<Self, CachingRedisError>;
+    async fn open<Info: IntoConnectionInfo + Send + Sync>(info: Info) -> Result<Self, CachingRedisError>;
     async fn set<K: ToRedisArgs + Send + Sync, V: ToRedisArgs + Send + Sync>(&self, key: K, value: V, duration: u64) -> Result<(), CachingRedisError>;
     async fn get<K: ToRedisArgs + Send + Sync, RV: FromRedisValue + Send + Sync>(&self, key: K) -> Result<Option<RV>, CachingRedisError>;
     async fn clear(&self) -> Result<(), CachingRedisError>;
 }
 
+#[derive(Clone, Debug)]
+pub struct SingleMultiplexedConnectionRedisClient {
+    pub connexion: MultiplexedConnection,
+}
+
 #[async_trait]
-impl RedisClient for redis::Client {
-    fn open<Info: IntoConnectionInfo>(info: Info) -> Result<Self, CachingRedisError> {
-        match Self::open(info) {
+impl RedisClient for SingleMultiplexedConnectionRedisClient {
+    async fn open<Info: IntoConnectionInfo + Send + Sync>(info: Info) -> Result<Self, CachingRedisError> {
+        let redis_client = match redis::Client::open(info) {
             Ok(client) => Ok(client),
             Err(error) => Err(CachingRedisError::CannotOpenConnection { description: format!("{}", error) })
-        }
+        }?;
+
+        let connection = match redis_client.get_multiplexed_async_connection_with_config(&AsyncConnectionConfig::new()).await {
+            Ok(connection) => connection,
+            Err(error) => return Err(CachingRedisError::CannotGetConnection { description: format!("{}", error) })
+        };
+
+        Ok(
+            SingleMultiplexedConnectionRedisClient {
+                connexion: connection
+            }
+        )
     }
 
     async fn set<K: ToRedisArgs + Send + Sync, V: ToRedisArgs + Send + Sync>(&self, key: K, value: V, duration: u64) -> Result<(), CachingRedisError> {
@@ -28,10 +43,7 @@ impl RedisClient for redis::Client {
             return Ok(());
         }
 
-        let mut connection = match self.get_multiplexed_async_connection_with_config(&AsyncConnectionConfig::new()).await {
-            Ok(connection) => connection,
-            Err(error) => return Err(CachingRedisError::CannotGetConnection { description: format!("{}", error) })
-        };
+        let mut connection = self.connexion.clone();
 
         let options = redis::SetOptions::default()
             .with_expiration(SetExpiry::EX(duration));
@@ -43,10 +55,7 @@ impl RedisClient for redis::Client {
     }
 
     async fn get<K: ToRedisArgs + Send + Sync, RV: FromRedisValue + Send + Sync>(&self, key: K) -> Result<Option<RV>, CachingRedisError> {
-        let mut connection = match self.get_multiplexed_async_connection_with_config(&AsyncConnectionConfig::new()).await {
-            Ok(connection) => connection,
-            Err(error) => return Err(CachingRedisError::CannotGetConnection { description: format!("{}", error) })
-        };
+        let mut connection = self.connexion.clone();
 
         match connection.get(key).await {
             Ok(value) => Ok(value),
@@ -55,10 +64,7 @@ impl RedisClient for redis::Client {
     }
 
     async fn clear(&self) -> Result<(), CachingRedisError> {
-        let mut connection = match self.get_multiplexed_async_connection_with_config(&AsyncConnectionConfig::new()).await {
-            Ok(connection) => connection,
-            Err(error) => return Err(CachingRedisError::CannotGetConnection { description: format!("{}", error) })
-        };
+        let mut connection = self.connexion.clone();
 
         match redis::cmd("FLUSHALL").exec_async(&mut connection).await {
             Ok(_) => Ok(()),
